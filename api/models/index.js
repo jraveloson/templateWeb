890539a6c4263901212efcedfcc680d67aa62157
Joanna Raveloson <joanna.raveloson.etu@unistra.fr>
2025-10-31 14:43:33 +0100

    controllers

--- api/models/index.js
+++ api/models/index.js
@@ -1,15 +1,15 @@
-const { Sequelize } = require ("sequelize");
-const { BDD }  = require ('../config');
+const { Sequelize } = require("sequelize");
+const { BDD } = require('../config');
 const sequelize = new Sequelize(`postgres://${BDD.user}:${BDD.password}@${BDD.host}/${BDD.bdname}`
-,{
+  , {
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
       ssl: true,
-      native:true
+      native: true
     },
-    define:  {
-    	timestamps:false
+    define: {
+      timestamps: false
     }
   });
 
@@ -18,6 +18,6 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.utilisateurs = require("./utilisateurs.model.js")(sequelize, Sequelize);
+db.pollution = require("./pollution.model.js")(sequelize, Sequelize);
 
 module.exports = db;
